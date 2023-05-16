93b873c9fa8b30b5c0d01bb899e2885b2d32400c
Niels <info@heimseiten.de>
2023-05-16 14:08:33 +0200

    Update HooksListener.php

--- src/Listener/HooksListener.php
+++ src/Listener/HooksListener.php
@@ -6,61 +6,62 @@ namespace Heimseiten\ContaoSpaceBundle\Listener;
 use Contao\FrontendTemplate;
 use Contao\Template;
 use Contao\Module;
+use Contao\StringUtil;  
 
 class HooksListener
 {
     public function __invoke(Template $template)
     {
         if ($template->type == 'article') {
-            if ( deserialize($template->space)[0] != NULL ) { 
+            if ( StringUtil::deserialize ($template->space)[0] != NULL ) { 
                 $template->class .= ' space_top';
-                if (is_numeric(deserialize($template->space)[0])){
-                    $template->style .= 'margin-top: ' . deserialize($template->space)[0] . 'px;'; 
+                if (is_numeric(StringUtil::deserialize ($template->space)[0])){
+                    $template->style .= 'margin-top: ' . StringUtil::deserialize ($template->space)[0] . 'px;'; 
                 } else {
-                    $template->style .= '--space_top: ' . deserialize($template->space)[0] . ';'; 
+                    $template->style .= '--space_top: ' . StringUtil::deserialize ($template->space)[0] . ';'; 
                 }
             }
-            if ( deserialize($template->space)[1] != NULL ) { 
+            if ( StringUtil::deserialize ($template->space)[1] != NULL ) { 
                 $template->class .= ' space_bottom';
-                if (is_numeric(deserialize($template->space)[1])){
-                    $template->style .= 'margin-bottom: ' . deserialize($template->space)[1] . 'px;'; 
+                if (is_numeric(StringUtil::deserialize ($template->space)[1])){
+                    $template->style .= 'margin-bottom: ' . StringUtil::deserialize ($template->space)[1] . 'px;'; 
                 } else {
-                    $template->style .= '--space_bottom: ' . deserialize($template->space)[1] . ';'; 
+                    $template->style .= '--space_bottom: ' . StringUtil::deserialize ($template->space)[1] . ';'; 
                 }
             }
-            if ( deserialize($template->space_mobile)[0] != NULL ) { 
+            if ( StringUtil::deserialize ($template->space_mobile)[0] != NULL ) { 
                 $template->class .= ' space_top_mobile'; 
-                $template->style .= '--space_top_mobile: ' . deserialize($template->space_mobile)[0] . ';'; 
+                $template->style .= '--space_top_mobile: ' . StringUtil::deserialize ($template->space_mobile)[0] . ';'; 
             }
-            if ( deserialize($template->space_mobile)[1] != NULL ) { 
+            if ( StringUtil::deserialize ($template->space_mobile)[1] != NULL ) { 
                 $template->class .= ' space_bottom_mobile'; 
-                $template->style .= '--space_bottom_mobile: ' . deserialize($template->space_mobile)[1] . ';'; 
+                $template->style .= '--space_bottom_mobile: ' . StringUtil::deserialize ($template->space_mobile)[1] . ';'; 
             }
         }
                
-            if ( deserialize($template->space)[0] != NULL ) { 
+            if ( StringUtil::deserialize ($template->space)[0] != NULL ) { 
                 $template->class .= ' space_top;'; 
-                if (is_numeric(deserialize($template->space)[0])){
-                    $template->style .= 'margin-top: ' . deserialize($template->space)[0] . 'px;'; 
+                if (is_numeric(StringUtil::deserialize ($template->space)[0])){
+                    $template->style .= 'margin-top: ' . StringUtil::deserialize ($template->space)[0] . 'px;'; 
                 } else {
-                    $template->style .= '--space_top: ' . deserialize($template->space)[0] . ';'; 
+                    $template->style .= '--space_top: ' . StringUtil::deserialize ($template->space)[0] . ';'; 
                 }
             }
-            if ( deserialize($template->space)[1] != NULL ) { 
+            if ( StringUtil::deserialize ($template->space)[1] != NULL ) { 
                 $template->class .= ' space_bottom'; 
-                if (is_numeric(deserialize($template->space)[1])){
-                    $template->style .= 'margin-bottom: ' . deserialize($template->space)[1] . 'px;'; 
+                if (is_numeric(StringUtil::deserialize ($template->space)[1])){
+                    $template->style .= 'margin-bottom: ' . StringUtil::deserialize ($template->space)[1] . 'px;'; 
                 } else {
-                    $template->style .= '--space_bottom: ' . deserialize($template->space)[1] . ';'; 
+                    $template->style .= '--space_bottom: ' . StringUtil::deserialize ($template->space)[1] . ';'; 
                 }
             }
-            if ( deserialize($template->space_mobile)[0] != NULL ) { 
+            if ( StringUtil::deserialize ($template->space_mobile)[0] != NULL ) { 
                 $template->class .= ' space_top_mobile'; 
-                $template->style .= '--space_top_mobile: ' . deserialize($template->space_mobile)[0] . ';'; 
+                $template->style .= '--space_top_mobile: ' . StringUtil::deserialize ($template->space_mobile)[0] . ';'; 
             }
-            if ( deserialize($template->space_mobile)[1] != NULL ) { 
+            if ( StringUtil::deserialize ($template->space_mobile)[1] != NULL ) { 
                 $template->class .= ' space_bottom_mobile'; 
-                $template->style .= '--space_bottom_mobile: ' . deserialize($template->space_mobile)[1] . ';'; 
+                $template->style .= '--space_bottom_mobile: ' . StringUtil::deserialize ($template->space_mobile)[1] . ';'; 
             }
         
     }
