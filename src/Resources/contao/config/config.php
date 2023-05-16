8568db8da86c3c0b7e3136a5d331985707dff42b
Niels <info@heimseiten.de>
2023-05-16 13:54:46 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,3 @@
 <?php
 
-if (TL_MODE == 'FE') {
-    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaospace/contao_space_bundle.scss|static';
-}
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaospace/contao_space_bundle.scss|static';
