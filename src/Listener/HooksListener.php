c49b96acd24c3dd1840de0fbd06465db971bc42c
Niels <info@heimseiten.de>
2023-05-25 14:58:36 +0200

    added twig

--- src/Listener/HooksListener.php
+++ src/Listener/HooksListener.php
@@ -38,43 +38,5 @@ class HooksListener
                 $template->style .= '--space_bottom_mobile: ' . StringUtil::deserialize ($template->space_mobile)[1] . ';'; 
             }
         }
-        if (    $template->type == 'text' || 
-                $template->type == 'image' ||
-                $template->type == 'headline' ||
-                $template->type == 'sliderStart' ||
-                $template->type == 'sliderStop' ||
-                $template->type == 'teaser_element' ||
-                $template->type == 'accordionSingle' ||
-                $template->type == 'gallery' ||
-                $template->type == 'youtube' ||
-                $template->type == 'html' ||
-                $template->type == 'colsetStart' ||
-                $template->type == 'player'
-            ) {       
-            if ( StringUtil::deserialize ($template->space)[0] != NULL ) { 
-                $template->class .= ' space_top;'; 
-                if (is_numeric(StringUtil::deserialize ($template->space)[0])){
-                    $template->style .= 'margin-top: ' . StringUtil::deserialize ($template->space)[0] . 'px;'; 
-                } else {
-                    $template->style .= '--space_top: ' . StringUtil::deserialize ($template->space)[0] . ';'; 
-                }
-            }
-            if ( StringUtil::deserialize ($template->space)[1] != NULL ) { 
-                $template->class .= ' space_bottom'; 
-                if (is_numeric(StringUtil::deserialize ($template->space)[1])){
-                    $template->style .= 'margin-bottom: ' . StringUtil::deserialize ($template->space)[1] . 'px;'; 
-                } else {
-                    $template->style .= '--space_bottom: ' . StringUtil::deserialize ($template->space)[1] . ';'; 
-                }
-            }
-            if ( StringUtil::deserialize ($template->space_mobile)[0] != NULL ) { 
-                $template->class .= ' space_top_mobile'; 
-                $template->style .= '--space_top_mobile: ' . StringUtil::deserialize ($template->space_mobile)[0] . ';'; 
-            }
-            if ( StringUtil::deserialize ($template->space_mobile)[1] != NULL ) { 
-                $template->class .= ' space_bottom_mobile'; 
-                $template->style .= '--space_bottom_mobile: ' . StringUtil::deserialize ($template->space_mobile)[1] . ';'; 
-            }
-        }
     }
 }
